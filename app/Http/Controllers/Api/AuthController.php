29135c5369503760fb8fd9a19ae389c0908d2901
AhmadMukminJamaludin <jcfirda@gmail.com>
2024-02-16 11:10:06 +0700

    validasi login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
@@ -14,15 +15,29 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $user = User::where('email', $request->email)->first();
-            $token = $user->createToken('token-name')->plainTextToken;
+        try {
+            if (Auth::attempt($credentials)) {
+                $user = User::where('email', $request->email)->first();
+                $token = $user->createToken('token-name')->plainTextToken;
 
-            return response()->json(['token' => $token], 200);
+                return response()->json([
+                    'message' => "Berhasil login",
+                    'data' => [
+                        'name' => $user['name'],
+                        'email' => $user['email'],
+                        'phone' => $user['phone'],
+                    ],
+                    'token' => $token
+                ], 200);
+            } else {
+                throw ValidationException::withMessages([
+                    'email' => ['The provided credentials are incorrect.'],
+                ]);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => "Gagal login, {$th->getMessage()}",
+            ], 422);
         }
-
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
     }
 }
